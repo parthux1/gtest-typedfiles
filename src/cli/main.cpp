76c434213875ba8bb9c775e983a84bde9dc9f561
Yann Arend Zankl <yann.a.zankl@gmail.com>
2024-05-04 01:25:56 +0200

    example

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,10 +1,13 @@
-//
-// Created by parthu on 5/3/24.
-//
+// own
+#include <include/ConfigurationFromYaml.hpp>
+// 3rd party
 #include <spdlog/spdlog.h>
 
-int main()
-{
-    spdlog::info("ping");
+int main() {
+
+    const auto config = Configuration::readFromYaml("../../../res/example_config.yaml");
+    spdlog::info("Config: {}", config.has_value() ? "valid" : "invalid");
+
+    spdlog::info(config->testSuites[0].generateGtestTypedef());
     return 0;
 }
