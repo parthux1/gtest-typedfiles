36cc4bac5a09fc6691a1c6800f2bed4447ddcf87
Yann Arend Zankl <yann.a.zankl@gmail.com>
2024-05-06 19:14:21 +0200

    log more stuff

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -3,11 +3,21 @@
 // 3rd party
 #include <spdlog/spdlog.h>
 
+constexpr auto TARGET_PATH = "/generated/";
+
 int main() {
 
     const auto config = Configuration::readFromYaml("../../../res/example_config.yaml");
     spdlog::info("Config: {}", config.has_value() ? "valid" : "invalid");
 
-    spdlog::info(config->testSuites[0].generateGtestTypedef());
+    for (const auto& suite : config->testSuites) {
+        spdlog::info("Suite: {}", suite.name);
+        for (const auto& file : suite.files) {
+            spdlog::info(file.generateMemberDeclaration());
+            spdlog::info(file.generateMemberDefinition());
+        }
+
+        spdlog::info(suite.generateGtestTypedef());
+    }
     return 0;
 }
