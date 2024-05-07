2483430421a1cc184a3916787cf56e424cb86829
Yann Arend Zankl <yann.a.zankl@gmail.com>
2024-05-08 01:26:26 +0200

    update with file from preset

--- tooling/init_vcpkg.sh
+++ tooling/init_vcpkg.sh
@@ -1,2 +1,7 @@
-./vcpkg/bootstrap-vcpkg.sh
-./vcpkg/vcpkg install
+#!/bin/bash
+# run from project root
+
+_VCPKG_PATH="tooling/vcpkg"
+
+./${_VCPKG_PATH}/bootstrap-vcpkg.sh
+./${_VCPKG_PATH}/vcpkg install
