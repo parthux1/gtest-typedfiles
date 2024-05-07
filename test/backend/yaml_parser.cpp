00a3ac40f31afe86eb7f71c20fe1991f0a5f1dea
Yann Arend Zankl <yann.a.zankl@gmail.com>
2024-05-08 00:08:57 +0200

    rename example file .txt -> .cpp

--- test/backend/yaml_parser.cpp
+++ test/backend/yaml_parser.cpp
@@ -1,9 +1,7 @@
 #include <ConfigurationFromYaml.hpp>
 #include <gtest/gtest.h>
 
-
-TEST(LoadYAML, ExampleFile)
-{
+TEST(LoadYAML, ExampleFile) {
     const auto config = Configuration::readFromYaml(std::string(PROJ_PATH) + "/res/example_config.yaml");
 
     ASSERT_TRUE(config.has_value()) << "Configuration::readFromYaml failed";
@@ -16,7 +14,7 @@ TEST(LoadYAML, ExampleFile)
     ASSERT_EQ(suite_one.files.size(), 1);
     const auto file_one = suite_one.files.at(0);
 
-    EXPECT_EQ(file_one.path, "file1.txt");
+    EXPECT_EQ(file_one.path, "file1.cpp");
 
     ASSERT_EQ(file_one.members.size(), 1);
     const auto member_one = *file_one.members.begin();
